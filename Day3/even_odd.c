1e0b56d33c20e5f65cff62d4b815321b7dcecf90
Deepak-132006 <deepakn132006@gmail.com>
2026-07-17 10:34:44 +0530

    Even / Odd

---
+++ Day3/even_odd.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    if (n % 2 == 0)
+    {
+        printf("%d is Even", n);
+    }
+    else
+    {
+        printf("%d is Odd", n);
+    }
+}
