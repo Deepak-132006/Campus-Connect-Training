783b625d543c46240ad1109243afa67942e6b560
Deepak-132006 <deepakn132006@gmail.com>
2026-07-23 09:43:52 +0530

    Zoho - 1

---
+++ Zoho/QuestionOne.java
@@ -0,0 +1,40 @@
+// Write a program to give the following output for the given input.
+// Eg 1: Input: a1b10
+//        Output: abbbbbbbbbb
+// Eg: 2: Input: b3c6d15
+//           Output: bbbccccccddddddddddddddd
+// The number varies from 1 to 99.
+
+package Zoho;
+
+import java.util.Scanner;
+
+public class QuestionOne {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        StringBuilder result = new StringBuilder();
+
+        int i = 0;
+
+        while (i < str.length()) {
+            char ch = str.charAt(i);
+            i++;
+
+            StringBuilder num = new StringBuilder();
+
+            while (i < str.length() && Character.isDigit(str.charAt(i))) {
+                num.append(str.charAt(i));
+                i++;
+            }
+
+            int count = Integer.parseInt(num.toString());
+
+            for (int j = 0; j < count; j++) {
+                result.append(ch);
+            }
+        }
+        System.out.println(result);
+    }
+}
