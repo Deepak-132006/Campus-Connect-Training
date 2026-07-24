645eec50bbe3f474586c35e2a018aa1c0f0964b4
Deepak-132006 <deepakn132006@gmail.com>
2026-07-24 11:06:59 +0530

    Deficient number

---
+++ Day8/DeficientNumber.java
@@ -0,0 +1,36 @@
+package Day8;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DeficientNumber {
+    public static ArrayList<Integer> remainder(int num) {
+        ArrayList<Integer> divisors = new ArrayList<>();
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                divisors.add(i);
+            }
+        }
+        return divisors;
+    }
+
+    public static void main(String[] args) {
+        int n = 0, sum = 0;
+        
+        System.out.print("Enter the number: ");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        ArrayList<Integer> divisors = remainder(n);
+
+        for (int num : divisors){
+            sum += num;
+        }
+        if(sum < n){
+            System.out.println("Entered number " + n + " is Deficient number");
+        } else {
+            System.out.println("Not an Deficient number");
+        }
+        sc.close();
+    }
+}
